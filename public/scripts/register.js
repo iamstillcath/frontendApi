794de9964235fd762d36f25caa81f72b14be8ac2
iamstillcath <catherine2lucia@gmail.com>
2022-06-21 01:40:06 +0100

    frontendApi

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -4,15 +4,16 @@ const register = (e) => {
   fetch("/user/signup", {
     method: "POST",
     headers: {
-      "content-type": "application/json",
+      Accept: "application/json, text/plain, */*", "Content-Type": "application/json"
     },
     body: JSON.stringify({
       name: document.querySelector(".name").value,
       email: document.querySelector(".email").value,
       password: document.querySelector(".pass").value,
+      password: document.querySelector(".compass").value,
       phoneNumber: document.querySelector(".phoneNumber").value,
       address: document.querySelector(".address").value,
-    }),
+    })
   })
     .then((res) => res.json())
     .then((data) => {
@@ -36,4 +37,40 @@ const register = (e) => {
     .catch();
 };
 
+
+const passvalid=document.querySelector(".pass");
+const compassvalid=document.querySelector(".compass");
+const compasserr=document.querySelector(".errMsg");
+
+compassvalid.addEventListener("mouseout",function(e) {
+  e.preventDefault()
+  
+
+  if(compassvalid.value === passvalid.value){
+    compasserr.innerHTML = "";
+}
+else{
+    compasserr.innerHTML = "password does not match";
+    compasserr.style.color = "red";
+}
+if(compassvalid.value === ""){
+    compasserr.innerHTML = "";
+}
+})
+
+// const errMsg=document.querySelector(".errMsg")
+// const phone =document.querySelector(".phoneNumber");
+// phone.addEventListener("change", function (e) {
+//   e.preventDefault()
+//   const pattern= /^(\+|00)[0-9]{1,3}[0-9]{4,14}(?:x.+)?$/
+//   if (phone.match(pattern)){
+//     errMsg.innerHTML=""
+//   }else {
+//      errMsg.innerHTML = "invalid phone number";
+//       errMsg.style.color = "red";
+//   }
+//   if(phone === ""){
+//     errMsg.innerHTML = "";
+// }
+// });
 document.querySelector(".signUp").addEventListener("click", register);
