a5ecb6de2f6a6871e0f36fba34451ebc9fca441b
iamstillcath <catherine2lucia@gmail.com>
2022-06-28 23:03:21 +0100

    frontendapi

--- app.js
+++ app.js
@@ -67,10 +67,10 @@ app.use((req, res, next) => {
   );
   res.header("Access-Control-Allow-Origin","true");
 
-  if (req.method == "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "Authorization","PUT, GET, POST, PATCH,DELETE,OPTIONS");
-    return res.status(200).json({});
-  }
+  // if (req.method == "OPTIONS") {
+  //   res.header("Access-Control-Allow-Methods", "Authorization","PUT, GET, POST, PATCH,DELETE,OPTIONS");
+  //   return res.status(200).json({});
+  // }
   next();
 });
 
