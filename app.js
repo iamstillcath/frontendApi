ba1647e252595b5cc19cb75759a1e6cee7241b36
iamstillcath <catherine2lucia@gmail.com>
2022-06-28 17:54:18 +0100

    frontendapi

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin","*");
   res.header(
-    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers","*/*",
     "Origin,X-Requested-With,Content-Type",
     "Accept",
     "Authorization"
